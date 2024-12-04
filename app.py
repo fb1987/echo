c429252c3054fc50ad411ad0909944d11688307c
fb1987 <frank.butty@gmail.com>
2024-12-03 22:15:13 -0500

    Update app.py for CORS

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import os
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import requests
 from dotenv import load_dotenv
 
 app = Flask(__name__)
+CORS(app)
 
 if __name__ == "__main__":
     port = int(os.getenv("PORT", 5000))  # Default to port 5000 for local testing
